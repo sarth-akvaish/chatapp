b76874978a36e7cf5c9f307a1f3f9db46d521233
Sarthak <sarthakvkumar@gmail.com>
2024-02-16 11:01:56 +0530

    Deployment code updated

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path';
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import dotenv from 'dotenv'
@@ -7,23 +8,28 @@ import userRoutes from './routes/user.routes.js'
 import connToDB from './db/conn.js';
 import { app, server } from './socket/socket.js'
 
-const port = 5000 || process.env.port;
+const port = process.env.port || 5000;
 dotenv.config();
+
+const __dirname = path.resolve();
+const parentDir = path.dirname(__dirname);
 
 app.use(express.json());
 app.use(cookieParser());
-
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messageRoutes);
 app.use('/api/users', userRoutes);
 
-// app.get('/', (req, res) => {
-//     res.send("Hey I am there")
-// })
+app.use(express.static(path.join(parentDir, "/frontend/dist")))
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(parentDir, "frontend", "dist", "index.html"));
+})
 
 
 server.listen(port, () => {
     connToDB();
+    console.log(parentDir);
     console.log(`Server is running Get some chill !! PORT : ${port}`)
 })
